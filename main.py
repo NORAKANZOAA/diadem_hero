de6411b941821960515a8dd6a109a22ba3ba18bc
Alycia Slimani <alyciaslimani@yahoo.fr>
2023-12-29 11:09:37 +0100

    check while

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
 from class_hero import Hero
 from class_monster import Monster
 from class_monster_healer import MonsterHealer
+
+random.choice = ([True, False])
 
 hero = Hero()
 monster = Monster()
@@ -17,17 +20,7 @@ def show_menu() -> int:
     else:
         show_menu()
 
-def show_menu() -> int:
-    print("\nC'est à votre tour de jouer, que souhaitez vous faire ?\n")
-    print("1 - Attaquer")
-    print("2 - Prendre une potion")
-    print("3 - Voir les stats")
-    print("4 - Quitter le jeu")
-    choice = input("\nVotre choix : ")
-    if(choice in ("1", "2", "3", "4")):
-        return int(choice)
-    else:
-        show_menu()
+
 
 def get_diademe():
     pass
@@ -58,37 +51,21 @@ while True:
     elif(choice == 4):
         # Quit game
         break
-while monster.life > 0 and hero.life > 0:
-    # Afficher le menu
-
-    # En fonction du choix faire des actions
-
-    # HEROS
-    #
-    # SI attaquer 
-    
-    # ALORS attaquer
-        
-    # SINON SI potion
-   
-    # ALORS soigner
-        
-
     # MONSTRE
-    #
-    # SI monstre healer
-        if isinstance(monster, MonsterHealer):
-            # ALORS SI il se soigne
-            if monster.heal():
-            #   ALORS soigner
-                        monster.heal()
-            #   SINON attaquer
-            else:
-                    monster.monster_attack()
-            # SINON attaquer directement
-        else:
-                    monster.monster_attack()
     
-        pass
+    # SI monstre healer
+    if isinstance(monster, MonsterHealer):
+            # ALORS SI il se soigne
+        if random.choice == True:
+            #   ALORS soigner
+            monster.heal()
+            #   SINON attaquer
+        else:
+             monster.monster_attack(hero)
+            # SINON attaquer directement
+    else:
+        monster.monster_attack(hero)
+    
+    pass
 
 print("Le jeu est terminé")
