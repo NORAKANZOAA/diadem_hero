0849686db25c28e37ffa6f04568b4d059d75e708
ferielag <aitgherbi.feriel@hotmail.fr>
2023-12-29 11:36:48 +0100

    ajouts monstres 2

--- class_monster_healer.py
+++ class_monster_healer.py
@@ -2,8 +2,6 @@ from class_monster import Monster
 from class_hero import Hero 
 
 class MonsterHealer(Monster):
-    def attack(self, hero):
-       hero.life -= 1
-    
+
     def healing(self):
         monster.life += 1
