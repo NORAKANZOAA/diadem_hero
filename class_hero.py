d226b843b9c07bfac4b2d0f951002f319da40041
Alycia Slimani <alyciaslimani@yahoo.fr>
2023-12-29 14:15:00 +0100

    finale version

--- class_hero.py
+++ class_hero.py
@@ -1,4 +1,4 @@
-from class_monster import Monster
+
 
 class Hero:
     def __init__(self, life = 5, potion = 3):
@@ -7,9 +7,14 @@ class Hero:
 
     
     def hero_attack(self,monster):
-        monster.life -= 1
+        damage = 1
+        monster.life -= damage
+        if monster.life > 0:
+            print(f"Vous avez attaqué un monster, il a perdu {damage} points de vie. Il lui reste {monster.life} .")
+        else:
+            print("Le monstre est mort, vous avez gagné!")
 
     def drink_potion(self):
         self.potion -= 1  
         self.life = 5
-
+        print(f"Vous avez bu une potion, il vous reste {self.potion} potion(s). Vous avez récupéré tous vos points de vie.")
