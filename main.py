c9f3a50f177f0e6177af33e6c88d06ddef4e5a31
Alycia Slimani <alyciaslimani@yahoo.fr>
2023-12-29 09:53:05 +0100

    boucle try

--- main.py
+++ main.py
@@ -9,7 +9,36 @@ def get_diademe():
 def check_game():
     pass
 
-while len(monsters) > 0:
+while len(monsters) > 0 and hero.life > 0:
+    # Afficher le menu
+
+    # En fonction du choix faire des actions
+
+    # HEROS
+    #
+    # SI attaquer 
+    if player_choice == 1:
+    # ALORS attaquer
+        hero.hero_attack()
+    # SINON SI potion
+    elif player_choice == 2:
+    # ALORS soigner
+        hero.drink_potion()
+
+    # MONSTRE
+    #
+    # SI monstre healer
+        if monster == monster_healer:
+    # ALORS SI il se soigne
+            if monster_healer.heal():
+    #   ALORS soigner
+                monster_healer.heal()
+    #   SINON attaquer
+            elif:
+            monster_healer.monster_attack()
+    # SINON attaquer directement
+        else:
+            monster.monster_attack()
     
     pass
 
