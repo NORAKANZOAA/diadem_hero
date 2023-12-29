0ec566a07a4788f8f103a1c7d03bd3623c3d5705
ferielag <aitgherbi.feriel@hotmail.fr>
2023-12-29 10:24:14 +0100

    monstre healer

--- class_monster_healer.py
+++ class_monster_healer.py
@@ -1,4 +1,16 @@
 from class_monster import Monster
+from class_hero import Hero 
 
 class MonsterHealer(Monster):
     pass
+# qu'est-ce que je dois faire? faire en sorte que le mosntre healer corresponde au monstre de base sauf que selon une certaine probabilité
+#il se soigne. 
+#dans la fonction attack, il faut qu'il puisse attaquer. Qui? Le hero. Si le hero l'attaque, il perd la moitié de ses vies. 
+    def attack(self, hero):
+    #le hero attaque puis le monstre attaque; lorsque le hero attaque, le monstre aura 1/2 de ses vies.
+    #on fait simplement un -1 sur le nombre de vies du heros 
+       hero.life -= 1
+    
+    def healing(self):
+        # une vie lui est restituée sur une probabilité donnée 
+        monster.life += 1
--- main.py
+++ main.py
@@ -10,7 +10,25 @@ def check_game():
     pass
 
 while len(monsters) > 0:
+    pass
+# Afficher le menu
+
+    # En fonction du choix faire des actions
+
+    # HEROS
+    #
+    # SI attaquer
+    # ALORS attaquer
+    # SINON SI potion
+    # ALORS soigner
+
+    # MONSTRE
+    #
+    # SI monstre healer
+    # ALORS SI il se soigne
+    #   ALORS soigner
+    #   SINON attaquer
+    # SINON attaquer directement
     
     pass
-
 print("Le jeu est terminé")
